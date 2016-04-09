b35db3f0a14168983f097bcfd2ca717bb82b8ca8
David Stephens <davidstephens92@gmail.com>
2016-04-09 11:29:20 -0700

    Update header to use Bootstrap (dekstop only)

--- scripts/header.js
+++ scripts/header.js
@@ -1,15 +1,15 @@
-function colorForIndex(index) {
+function colorClassForIndex(index) {
     switch(index % 10) {
-        case 0: return "black";
-        case 1: return "lightgray";
-        case 2: return "#3f51b5";
-        case 3: return "#ffeb3b";
-        case 4: return "#d32f2f";
-        case 5: return "#388e3c";
-        case 6: return "#ec407a";
-        case 7: return "goldenrod" /*"#ffc107"*/;
-        case 8: return "#e65100";
-        case 9: return "#880e4f";
+        case 0: return "zero-color";
+        case 1: return "one-color";
+        case 2: return "two-color";
+        case 3: return "three-color";
+        case 4: return "four-color";
+        case 5: return "five-color";
+        case 6: return "six-color";
+        case 7: return "seven-color";
+        case 8: return "eight-color";
+        case 9: return "nine-color";
     }
 }
 
@@ -26,12 +26,13 @@ function drawHeader(targetDiv) {
     
     for (var i = 0; i < numOfDigits; i++) {
         var digitDiv = document.createElement("div");
-        digitDiv.classList.add("digit-div");   
+        digitDiv.classList.add("digit");  
+        digitDiv.classList.add("no-select"); 
         digitDiv.style.height = (digitWidth).toString() + "px";
         digitDiv.style.width = (digitWidth).toString() + "px";
         
         digitDiv.innerText = (i % 10).toString();
-        digitDiv.style.color = colorForIndex(i);
+        digitDiv.classList.add(colorClassForIndex(i));
         
         targetDiv.appendChild(digitDiv);
     }
